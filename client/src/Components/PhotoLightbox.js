4b1d3c0a78b4d8227ab5fdf845db68105f61e3a9
Margo Manachenko <r.manachenko@gmail.com>
2018-11-27 13:16:44 +0200

    Test loading

--- client/src/Components/PhotoLightbox.js
+++ client/src/Components/PhotoLightbox.js
@@ -2,6 +2,7 @@ import React from 'react';
 import close from '../public/close.svg';
 import {Transition} from 'react-transition-group';
 
+
 const PhotoLightbox =(props)=>(
     <Transition timeout={100} in={props.show} appear>
         {(status => (
@@ -10,7 +11,7 @@ const PhotoLightbox =(props)=>(
             <img className="icon-close-lightbox" src={close}  alt=""/>
             {/*<div className="photo">*/}
                 {/*<img className="bigPhoto" src={props.currentPictureBig1x} srcSet={`${props.currentPictureBig2x} 2x, ${props.currentPictureBig3x} 3x`} alt=""/>*/}
-                <img className="bigPhoto loading" src={props.currentPictureBig} alt=""/>
+                <img className="bigPhoto loading" src={props.currentPictureBig} alt="" onLoad={()=>console.log('loaded')}/>
             {/*</div>*/}
         </div>
     </div>
